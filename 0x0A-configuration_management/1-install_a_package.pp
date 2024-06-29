dccdf8780cec3d6940f2c14ce4a9345faa697923
med099 <138779714+med099@users.noreply.github.com>
2024-06-29 01:16:04 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-#!/usr/bin/pup
-# Install flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#Install flask (2.1.0)
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
+exec {'python-installed':
+command => '/usr/bin/which python3'
 }
